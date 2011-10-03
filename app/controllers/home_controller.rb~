b12cfd2e74dfb5a70d08e4ba4e82f6158b599c2b
Eli Parra <eli@elzr.com>
2011-10-03 17:19:10 -0500

    init

---
+++ app/controllers/home_controller.rb~
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+	@set = params[:set]
+	@size = params[:size]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsShoebox::Application.config.secret_token = '824f3b7df25a047aca9017ec68410bba2b91aa5ca4df21549f25eded4e627547f8ee1a0b75f58b28bc36ef4a10eada13d6d59fac1078a056da37ced13e8663d3'
